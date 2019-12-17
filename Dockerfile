c1e1a69a2cb16627ef2ac16af5c8ea474645331d
lewis <lewiskosh@gmail.com.com>
2019-12-17 14:39:38 +0300

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM golang:1.13.5-alpine3.10
+
+WORKDIR /pitjet
+COPY . .
---
+++ Server.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+)
+
+const (
+	protocol string = "tcp"
+	host     string = "127.0.0.1"
+	port     string = "81"
+)
+
+func main() {
+	// boot up the tcp server
+	listener, err := net.Listen(protocol, host+":"+port)
+	if err != nil {
+		log.Fatal("tcp server failiure: ", err)
+	}
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Fatal("TCP server accept error:", err)
+		}
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	bufferBytes, err := bufio.NewReader(conn).ReadBytes('\n')
+
+	if err != nil {
+		log.Println("Connection broken by client..")
+		conn.Close()
+		return
+	}
+
+	message := string(bufferBytes)
+	clientAddress := conn.RemoteAddr().String()
+	response := fmt.Sprintf(message + "from " + clientAddress + "\n")
+	log.Println(response)
+	conn.Write([]byte("you sent: " + response))
+
+	handleConnection(conn)
+}
